8e61fffb6650c6cf32f5f00ae87708c296525c87
allamurodxakimov <xakimov2806@gmail.com>
2023-10-05 18:36:42 -0700

    list 9

--- List09.py
+++ List09.py
@@ -6,4 +6,14 @@ def main(list1):
     Returns:
         bool: return answer
     """
-    return
+    c=0
+    a=0
+    while a<len(list1)-1:
+        if list1[a]==list1[a-1]:
+            c+=1
+        a+=1   
+    if len(list1)-1==c:
+        return True
+    else:return False
+print(main(list1=[0, 0, 0, 0, 0]))
+print(main(list1=['x', 'x', 'y', 'y', 'z']))
